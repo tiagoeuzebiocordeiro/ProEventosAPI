a5545abf5f93ee7c1cdad2dcf3b9992c56313eab
Tiago Cordeiro <tiagoezc@hotmail.com>
2023-08-15 23:10:33 -0300

    Binding + Filtro grid + Ajuste Layout

--- ProEventosAPI/Front/ProEventos-App/src/app/app.module.ts
+++ ProEventosAPI/Front/ProEventos-App/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProEventosAPI/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventosAPI/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,27 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 50;
+  marginImg: number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista
+  }
+
+  public set filtroLista(value: any) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string) {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (      evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http:HttpClient) { } // Referencia ao import http
 
@@ -16,9 +37,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImagem() {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos() : void {
     this.http.get('https://localhost:7237/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+
+      },
       error => console.log(error),
     );
   }
